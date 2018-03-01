47da1050a2e05c4f13c420c9bf6466df6b003feb
Angela Maria Alvarez <angelaalhe@unisabana.edu.co>
2018-03-01 11:10:29 -0500

    Finalizada

---
+++ Angela Alvarez H - MountFuji/Assets/Train.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : MonoBehaviour {
+	public float speedz;
+	public float speedy;
+	public float speedx;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (speedx, speedy, speedz);
+	}
+}
